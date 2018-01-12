5c27f9d4d4aa7a1589fb17bbceddfcd38c49a110
zamgi <denisov72@mail.ru>
2018-01-12 22:25:53 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,21 @@
+del "*.suo" /S/Q/F/A
+del "*.csproj.user" /S/Q/F/A
+del "syntax_ru.web.demo\bin\*.pdb" /Q
+rd "syntax_ru.web.demo\obj" /S/Q
+rd "syntax_ru.web.demo\(logs)" /S/Q
+rem del "syntax_ru.console.demo\bin\*.pdb" /Q
+rem rd "syntax_ru.console.demo\obj" /S/Q
+rd "captcha\bin" /S/Q
+rd "captcha\obj" /S/Q
+rd "crfsuite.net\bin" /S/Q
+rd "crfsuite.net\obj" /S/Q
+rd "lingvo.core\bin" /S/Q
+rd "lingvo.core\obj" /S/Q
+rd "pos-tagger-crf.net\bin" /S/Q
+rd "pos-tagger-crf.net\obj" /S/Q
+rd "syntax-crf.net\bin" /S/Q
+rd "syntax-crf.net\obj" /S/Q
+rd "SentSplitter\bin" /S/Q
+rd "SentSplitter\obj" /S/Q
+rd "morphology\bin" /S/Q
+rd "morphology\obj" /S/Q
